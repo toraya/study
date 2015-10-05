5c8b3eecb23d8bb88c85976431fdbda2cbcf7a2d
OhnumaRina <rina@OhnumaRina-no-MacBook-Pro.local>
2015-10-05 14:25:34 +0900

    Initial Commit

---
+++ ResSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ResSample
+//
+//  Created by OhnumaRina on 2015/10/05.
+//  Copyright (c) 2015年 OhnumaRina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ResSample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ResSample
+//
+//  Created by OhnumaRina on 2015/10/05.
+//  Copyright (c) 2015年 OhnumaRina. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
